d2dcdf99d29d0a5f9187bb9a15ba61050951b033
G.H Geng <i4sadqqc@gmail.com>
2024-12-17 12:57:30 +0800

    更新內容: 使程式碼更具一致性以便維護及開發

--- ecommerce_web/ecommerce_web/src/main/java/com/i4/ecommerce_web/controller/UserController.java
+++ ecommerce_web/ecommerce_web/src/main/java/com/i4/ecommerce_web/controller/UserController.java
@@ -4,7 +4,6 @@ import com.i4.ecommerce_web.pojo.Result;
 import com.i4.ecommerce_web.pojo.User;
 import com.i4.ecommerce_web.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +14,11 @@ import java.util.Map;
 @Slf4j
 @RequestMapping("api/user")
 public class UserController {
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
     /**
      *
      * 註冊會員
